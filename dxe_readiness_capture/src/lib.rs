ebe6fe8dfc4d35efc9b6f3366b1b71159ec96f48
Michael Kubacki <michael.kubacki@microsoft.com>
2025-09-04 11:25:00 -0400

    Switch to Apache 2.0 License
    Per https://github.com/OpenDevicePartnership/governance/blob/main/rfc/0013-patina-apache-20-license.md,
    switch the patina-readiness-tool repo to the Apache 2.0 License.
    
    Signed-off-by: Michael Kubacki <michael.kubacki@microsoft.com>

--- dxe_readiness_capture/src/lib.rs
+++ dxe_readiness_capture/src/lib.rs
@@ -4,7 +4,7 @@
 //!
 //! Copyright (C) Microsoft Corporation. All rights reserved.
 //!
-//! SPDX-License-Identifier: BSD-2-Clause-Patent
+//! SPDX-License-Identifier: Apache-2.0
 //!
 
 // no_std and no_main are applicable only when building as an EFI application.
